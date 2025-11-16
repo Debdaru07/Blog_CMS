6c5a5562d0b74b880989115cdb4d8ff0b04e7534
Debdaru Dasgupta <debdarudasgupta0799@gmail.com>
2025-11-17 01:22:54 +0530

    Notification button aligned

--- client/src/components/layout/Topbar.jsx
+++ client/src/components/layout/Topbar.jsx
@@ -1,23 +1,19 @@
 import React from "react";
 
 const Topbar = ({ title }) => (
-  <header className="flex items-center justify-between border-b border-black/10 px-10 py-4 bg-background-light">
-    <h2 className="text-black text-lg font-bold tracking-tight">{title}</h2>
-
-    <div className="flex items-center gap-4">
-      <button className="rounded-full h-10 w-10 bg-black/5 hover:bg-black/10 flex items-center justify-center transition">
+  <header className="flex items-center whitespace-nowrap border-b border-black/10 px-10 py-4 bg-background-light">
+    <h2 className="text-black text-lg font-bold leading-tight tracking-[-0.015em]">
+      {title}
+    </h2>
+    <div className="flex flex-1 justify-end gap-4 items-center">
+      <button className="flex cursor-pointer items-center justify-center 
+                         overflow-hidden rounded-full h-10 w-10 
+                         bg-black/5 text-black 
+                         hover:bg-black/10 transition-colors duration-200">
         <span className="material-symbols-outlined text-xl">
           notifications
         </span>
       </button>
-
-      <div
-        className="rounded-full bg-cover bg-center size-10"
-        style={{
-          backgroundImage:
-            "url('https://lh3.googleusercontent.com/aida-public/AB6AXuC-DTnIvGpPZW487XzCWSr6_c1PCmdqL85hNufbcutFl5an4DJMAlQ9rfvi7Yotz5Wwj55w5zHPA0Mbxz2yGhVyEdzNp1kY-wCgDipL8KdxZqdNXvMhMe3NjHB9yssOqM7ALvgS48DTyqXZksvOEHf1C5lSqYlZW3TgcITDKUbomBNcHxlS-oPJ8ktIUr4_Gb0ce9m1eRKMVOVVIV38zeVvQ00WdXSRQ_bM2UJsjGehSR3bl21_T-sLOVhhIEwCqxBOY8-2pTpHRDk8');",
-        }}
-      />
     </div>
   </header>
 );
